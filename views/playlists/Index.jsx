cdd134d239921947b7c35703ddd07c09027c7c7c
Josue <cuellarjosue3@gmail.com>
2022-10-26 11:02:52 -0700

    Created Index.jsx

---
+++ views/playlists/Index.jsx
@@ -0,0 +1,35 @@
+const React = require('react')
+
+class Index extends React.Component {
+    render(){
+        const { playlists } = this.props
+        return(
+            <ul>
+               {
+                playlists.map((playlist) => {
+                    const { title, artist, favoriteSong, _id} = playlist
+                    return(
+                        <li key={_id}>
+                            <a href={`/playlists/${_id}`}>
+                                {title}
+                            </a> is {Artist}
+                            <br />
+                            {
+                                favoriteSong ?
+                                'It\'s added to favorites playlist':
+                                'It\'s not added to favorites playlist'
+                            }
+                            <br />
+                            <form method="POST" action={`/playlists/${_id}?_method=DELETE`}>
+                                <input type="submit" value={`Delete ${artist} ${title}`} />
+                            </form>
+
+                        </li>
+                    )
+                })
+               }
+            </ul>
+        )
+    }
+}
+module.exports = Index
