b9de09237d33fe82b5f64994adc8e4640668dbf8
Josue <cuellarjosue3@gmail.com>
2022-10-26 11:36:48 -0700

    Created Show.jsx

---
+++ views/playlists/Show.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+
+class Show extends React.Component {
+    render(){
+        const { title, artist, favoriteSong} = this.props.playlist
+        const capTitle = title[0].toUpperCase() + title.substring(1)
+        return(
+            <p>{capTitle} is by {artist} and {favoriteSong ? 'it\'s on favorites' : 'it\'s not on favorites'}</p>
+        )
+    }
+}
+module.exports = Show
