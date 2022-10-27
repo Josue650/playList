c2372e41b610438945cdd4d17edc34374ff38d28
Josue <cuellarjosue3@gmail.com>
2022-10-26 17:24:22 -0700

    Created models

---
+++ models/playlist.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+// Make a Schema
+const playlistSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    artist: { type: String, required: true },
+    favoriteSong: Boolean
+})
+
+// Make a model from the Schema
+const Playlist = mongoose.model('Playlist', playlistSchema)
+
+// Export the Model for the use in the App
+
+module.exports = Playlist
